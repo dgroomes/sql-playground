df384d276dd168d539991da16775f83cf6cf6672
David Groomes <dwgroomes@gmail.com>
2020-11-07 11:37:43 -0600

    Identify transition rows

--- snapshots/snapshots-explore.sql
+++ snapshots/snapshots-explore.sql
@@ -1,6 +1,5 @@
 select * from snapshots limit 10;
 
--- WORK IN PROGRESS
 -- Contrast the *current* value of "groundhog_appeared" with its *immediately preceding* value
 --
 -- The `lag` function fits exactly this requirement. Interestingly, though, the "lag" function can only be used in the
@@ -17,3 +16,27 @@ select * from snapshots limit 10;
 -- 2020-11-05	true	true
 select date, groundhog_appeared, lag(groundhog_appeared) over (partition by 1) as groundhog_appeared_yesterday from snapshots order by date;
 
+-- Building off of the "lag" pattern we discovered in the previous query, let's identify rows where a groundhog_appeared
+-- "transition" occurred. By transition, I mean: is today's value of "groundhog_appear" different from the previous day?
+-- Rather, did the value *transition*?
+--
+-- The result set looks like this:
+-- date	transition
+-- 2020-11-02	the groundhog disappeared
+-- 2020-11-04	the groundhog appeared 🐿
+--
+-- This is what we expect. The groundhog disappeared on 11/2 so we see a record for that date. The groundhog continued
+-- to not appear on 11/3 so we do not see a record on that date because that is not a transition. Then, on 11/4 the
+-- groundhog appeared and we see a record.
+with lag_adorned as (
+    select date, groundhog_appeared, lag(groundhog_appeared) over (partition by 1) as groundhog_appeared_yesterday from snapshots order by date),
+transitions as (
+    select * from lag_adorned where groundhog_appeared != groundhog_appeared_yesterday),
+synthesized as (
+    select date,
+           case when
+               groundhog_appeared then 'the groundhog appeared 🐿'
+               else 'the groundhog disappeared'
+           end as transition
+    from transitions)
+select * from synthesized;
--- snapshots/snapshots.md
+++ snapshots/snapshots.md
@@ -1,12 +1,7 @@
 # snapshots
 
-NOT YET IMPLEMENTED
-
-* Schema file: `snapshots-schema.ddl`
-* Data file: `snaphshots-data.sql`
-
-I'm trying to learn how to do a query where each row needs to be compared to the row predeceding it. I think I need to
-use `window` and I'm hoping that works! If that doesn't work then I might explore scripting in the query with PL/pgSQL.
+Showcasing the `lag` Postgres function which is a part of Postgres' windowing support. Specifically, I wanted to
+illustrate a query where each row needs to be compared to the row preceding it.
 
 ### Instructions
 
@@ -14,7 +9,6 @@ use `window` and I'm hoping that works! If that doesn't work then I might explor
 psql --username postgres --host localhost -f snapshots/snapshots-schema.ddl 
 psql --username postgres --host localhost -f snapshots/snapshots-data.sql
 ```
-
 
 ### Referenced Material
 
