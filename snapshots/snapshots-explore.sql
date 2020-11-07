8153eff117161241f98247be54a62ddaed183bef
David Groomes <dwgroomes@gmail.com>
2020-11-07 11:18:49 -0600

    Use the "lag" function to contrast rows

--- snapshots/snapshots-explore.sql
+++ snapshots/snapshots-explore.sql
@@ -1,1 +1,19 @@
 select * from snapshots limit 10;
+
+-- WORK IN PROGRESS
+-- Contrast the *current* value of "groundhog_appeared" with its *immediately preceding* value
+--
+-- The `lag` function fits exactly this requirement. Interestingly, though, the "lag" function can only be used in the
+-- context of a window operation but we don't have any logical requirement to split up the table in different windows.
+-- So, to work around this Postgres technical constraint, we just apply a global window equal to the whole table by
+-- doing `partition by 1`.
+--
+-- The result set looks like this:
+-- date	groundhog_appeared	groundhog_appeared_yesterday
+-- 2020-11-01	true
+-- 2020-11-02	false	true
+-- 2020-11-03	false	false
+-- 2020-11-04	true	false
+-- 2020-11-05	true	true
+select date, groundhog_appeared, lag(groundhog_appeared) over (partition by 1) as groundhog_appeared_yesterday from snapshots order by date;
+
--- snapshots/snapshots.md
+++ snapshots/snapshots.md
@@ -13,4 +13,10 @@ use `window` and I'm hoping that works! If that doesn't work then I might explor
 ```
 psql --username postgres --host localhost -f snapshots/snapshots-schema.ddl 
 psql --username postgres --host localhost -f snapshots/snapshots-data.sql
-```   
+```
+
+
+### Referenced Material
+
+* [Postgres official docs: "Window Functions" tutorial](https://www.postgresql.org/docs/13/tutorial-window.html)
+* [Postgres official docs: "Window Functions" reference](https://www.postgresql.org/docs/13/functions-window.html)
